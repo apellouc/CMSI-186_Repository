67fbda903559c8fd81fee9f82d7083c567815993
Amy Pellouchoud <apellouc@lion.lmu.edu>
2017-03-17 12:42:46 -0700

    Updated fixed ClockSolver.java

--- CMSI-186_Projects/Homework 04/ClockSolver.java
+++ CMSI-186_Projects/Homework 04/ClockSolver.java
@@ -1,6 +1,6 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  File name     :  ClockSolver.java
- *  Purpose       :  Main Program for use with Clock.java to
+ *  Purpose       :  Main Program for use with Clock.java to list times that analog clock hands make an angle
  *  @author       :  Amy Pellouchoud
  *  Date written  :  2017-03-02
  *  Description   :  Calculates all times at which an inputted angle between hour and minute hand
@@ -9,6 +9,7 @@
  *  Notes         :  None
  *  Warnings      :  None
  *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
+                     NumberFormatException when the input string is not a number
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  Revision History
  *  ----------------
@@ -17,11 +18,8 @@
  *  @version 1.0.0  2017-02-28  B.J. Johnson   Initial writing and release
  *  @version 1.2.0  2017-03-14  A. Pellouchoud Wrote main program
  *  @version 1.3.0  2017-03-15  A. Pellouchoud Finished program
+ *  @version 1.4.0  2017-03-16  A. Pellouchoud Revised loop, fixed program
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-//for every minute, angle changes 5.5 degrees
-//0.0083 degrees per second
-
 
 import java.util.Arrays;
 
@@ -30,19 +28,18 @@ public class ClockSolver {
    /**
     *  Definitions
     */
-    private static final double SECONDS_IN_DAY = 86400;
+    private static final double SECONDS_IN_TWELVE_HOURS = 43200;
     private static final double SECONDS_IN_MINUTE = 60;
     private static final int MINUTES_IN_HOUR = 60;
+    private static final double MAX_HOUR_VALUE = 11;
+    //Degree range for hand angle to match input angle
+    private static final int DEGREE_RANGE = 3;
+
 
    /**
     *  Main program starts here
     */
    public static void main( String args[] ) {
-
-      System.out.println( "\nWelcome to Clock Solver!" );
-      System.out.println( "--------------------------" );
-
-      System.out.println( "Args: " + Arrays.toString(args) );
 
       /**
        *  Check validity of the args
@@ -51,11 +48,19 @@ public class ClockSolver {
          throw new ArrayIndexOutOfBoundsException( "You must send either 1 or 2 args." );
       }
 
-      Clock.validateAngleArg( args[0] );
+      try { Clock.validateAngleArg( args[0] ); }
+      catch ( NumberFormatException nfe ) { System.out.println( "\nInput string is not a number" ); System.exit(1); }
+      catch ( IllegalArgumentException iae ) { System.out.println( iae.toString() ); System.exit(1); }
 
       if ( args.length == 2 ) {
-         Clock.validateTimeSliceArg( args[1] );
+         try { Clock.validateTimeSliceArg( args[1] ); }
+         catch ( NumberFormatException nfe ) { System.out.println( "\nInput string is not a number" ); System.exit(1); }
+         catch ( IllegalArgumentException iae ) { System.out.println( iae.toString() ); System.exit(1); }
       }
+
+      System.out.println( "\nWelcome to Clock Solver!" );
+      System.out.println( "--------------------------" );
+      System.out.println( "Args: " + Arrays.toString(args) );
 
       /**
        * Convert args[0], the angle, to a double and prints out the input.
@@ -83,16 +88,22 @@ public class ClockSolver {
        *  Increments through the clock by the given timeSlice
        *  If the hand angle of the clock after the tick is equal to the inputted angle, prints the time
        */
-       System.out.println( "\nList of values where Hands make angle " + angle + ":" );
+       System.out.println( "\nList of times where hands make angle " + angle + ":" );
+       System.out.println( "\n***note: Times may be within " + DEGREE_RANGE + " degrees of " + angle + "***\n" );
 
        //Main loop
-       for ( int i = 0;
-             ( ( clock.getMinuteHand() * SECONDS_IN_MINUTE ) + ( clock.getHourHand() * MINUTES_IN_HOUR * SECONDS_IN_MINUTE ) ) < SECONDS_IN_DAY;
-             clock.tick() ) {
-         if ( angle == clock.getHandAngle() ) {
+       while ( clock.getHourHand() <= MAX_HOUR_VALUE ) {
+
+          double handAngle = clock.getHandAngle();
+
+         if (( Math.abs( angle - handAngle ) <= DEGREE_RANGE ) ||
+             ( Math.abs( angle - ( 360 - handAngle ) ) <= DEGREE_RANGE )) {
             System.out.println( clock );
-         } else continue;
+            clock.tick();
+         } else clock.tick();
       }
+
+      System.exit(0);
 
    }
 }
