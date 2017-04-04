18ca629f4fdcdbcccb804f33824713c624a2eb59
Amy Pellouchoud <apellouc@lion.lmu.edu>
2017-04-03 22:28:30 -0700

    Update Pole.java

--- CMSI-186_Projects/Homework05/Pole.java
+++ CMSI-186_Projects/Homework05/Pole.java
@@ -15,8 +15,11 @@
  *  @version 1.0.0  2017-03-23  A. Pellouchoud  Initial writing and release
  *  @version 1.1.0  2017-03-28  A. Pellouchoud  Wrote constructor and get/set pole position methods
  *  @version 1.2.0  2017-03-30  A. Pellouchoud  Added a toString method, wrote tests
+ *  @version 1.3.0  2017-04-03  A. Pellouchoud  Cleaned up program, finished test methods
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+import java.text.DecimalFormat;
 
 public class Pole {
 
@@ -38,28 +41,14 @@ public class Pole {
    /**
    *  Method to get the pole's x position
    *  @return double-precision value of the pole's xpos
-   *  @throws
    */
    public double getPoleXPOS() { return xpos; }
 
    /**
    *  Method to get the pole's y position
    *  @return double-precision value of the pole's ypos
-   *  @throws
    */
    public double getPoleYPOS() { return ypos; }
-
-   /**
-   *  Method to set the pole position
-   *  @param x new xpos
-   *  @param y new ypos
-   *  @throws
-   */
-   public void setPolePosition( double x, double y ) {
-
-      xpos = x;
-      ypos = y;
-   }
 
    /**
     *  Method to print the pole's information to a string
@@ -67,19 +56,24 @@ public class Pole {
     */
    public String toString() {
 
-     return "Position: (" + xpos + "," + ypos + ")";
+      DecimalFormat df = new DecimalFormat( "#0.00" );
+
+     return "Position: (" + df.format(xpos) + ", " + df.format(ypos) + ")";
    }
 
-
+   //Main program for testing
    public static void main( String args[] ) {
 
-      Pole p1 = new Pole( 0, 0 );
-      System.out.println( p1.toString() );
+      Pole p1 = new Pole( 1, 2 );
+      System.out.println( "Pole initialized, expecting (1, 2): " + p1.toString() );
 
-      System.out.println( "\n  Testing setPolePosition()....");
-      p1.setPolePosition( 1, 2 );
-      System.out.println( "Expecting new pole position (1, 2): " + p1.toString() );
+      //Test getBallXPOS()
+      System.out.println( "Testing getPoleXPOS() :");
+      System.out.println( "Expecting 1.0: " + p1.getPoleXPOS() );
 
+      //Test getBallYPOS
+      System.out.println( "\nTesting getPoleYPOS() :");
+      System.out.println( "Expecting 2.0: " + p1.getPoleYPOS() );
    }
 
 }
