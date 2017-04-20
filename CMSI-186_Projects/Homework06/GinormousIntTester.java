c1920aa240b2efc5bde56b653253257ac7c70916
Amy Pellouchoud <apellouc@lion.lmu.edu>
2017-04-20 12:28:33 -0700

    Create GinormousIntTester.java
    Incomplete

---
+++ CMSI-186_Projects/Homework06/GinormousIntTester.java
@@ -0,0 +1,108 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  GinormousIntTester.java
+ *  Purpose       :
+ *  @author       :  Amy Pellouchoud
+ *  Date written  :  2017-03-21
+ *  Description   :
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ----------------
+ *            Rev      Date     Modified by:    Reason for change/modification
+ *           -----  ----------  ------------    -----------------------------------------------------------
+ *  @version 1.0.0  2017-04-05  B.J. Johnson    Initial writing and release
+ *  @version 1.1.0  2017-04-06  A. Pellouchoud  Initial Writing and Release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+ public class GinormousIntTester {
+
+    public GinormousIntTester() {
+       super();
+    }
+
+    public static void main( String[] args ) {
+       GinormousIntTester git = new GinormousIntTester();
+
+       System.out.println( "\n  Hello from the GinormousIntTester program!!" );
+
+       System.out.println( "\n    =============================================\n" );
+
+       System.out.println( "    ~~~ Tests for constructor & toString() ~~~ \n");
+       System.out.println( "    Making a new GinormousInt g1: " );
+       GinormousInt g1 = new GinormousInt( "144127909719710664015092431502440849849506284148982076191826176553" );
+       System.out.println( "      expecting: 144127909719710664015092431502440849849506284148982076191826176553\n" +
+                           "        and got: " + g1.toString() );
+
+       System.out.println( "\n    Making a second new GinormousInt g2 [same as first]: " );
+       GinormousInt g2 = new GinormousInt( "144127909719710664015092431502440849849506284148982076191826176553" );
+       System.out.println( "      expecting: 144127909719710664015092431502440849849506284148982076191826176553\n" +
+                           "        and got: " + g2.toString() );
+
+       System.out.println( "\n    Making a third new GinormousInt g3 [differs at position 47    |]: " +
+                           "\n                                                               v   " );
+       GinormousInt g3 = new GinormousInt( "144127909719710664015092431502440849849506284108982076191826176553" );
+       System.out.println( "      expecting: 144127909719710664015092431502440849849506284108982076191826176553\n" +
+                                               "        and got: " + g3.toString() );
+
+       System.out.println( "\n    Making a fourth new GinormousInt g4 [same as g3 but truncated]: "  );
+       GinormousInt g4 = new GinormousInt( "144127909719710664015092431502440849849506284" );
+       System.out.println( "      expecting: 144127909719710664015092431502440849849506284\n" +
+                           "        and got: " + g4.toString() );
+
+       System.out.println( "\n    Making a fifth new GinormousInt g5 [same as g1 but negative]: "  );
+       GinormousInt g5 = new GinormousInt( "-144127909719710664015092431502440849849506284148982076191826176553" );
+       System.out.println( "      expecting: -144127909719710664015092431502440849849506284148982076191826176553\n" +
+                           "        and got: " + g5.toString() );
+
+       System.out.println( "\n    Making a sixth new GinormousInt, checking GinormousInt.ZERO: "  );
+       GinormousInt g6 = new GinormousInt( "0" );
+       System.out.println( "      expecting: " + GinormousInt.ZERO + "\n" +
+                           "        and got: " + g6.toString() );
+
+       System.out.println( "\n    Making a seventh new GinormousInt, checking GinormousInt.ONE: "  );
+       GinormousInt g7 = new GinormousInt( "1" );
+       System.out.println( "      expecting: " + GinormousInt.ONE + "\n" +
+                           "        and got: " + g7.toString() );
+
+       System.out.println( "\n    Making a eighth new GinormousInt, checking GinormousInt.TEN: "  );
+       GinormousInt g8 = new GinormousInt( "10" );
+       System.out.println( "      expecting: " + GinormousInt.TEN + "\n" +
+                           "        and got: " + g8.toString() );
+
+       System.out.println( "\n    =============================================\n" );
+
+       System.out.println( "      ~~~ Tests for equals() ~~~ ");
+       System.out.println( "\n    Comparing equality of g1 and g2: " );
+       System.out.println( "      expecting: true\n" + "        and got: " + g1.equals( g2 ) );
+
+       System.out.println( "\n    Comparing equality of g1 and g3 [detect different digit]: " );
+       System.out.println( "      expecting: false\n" + "        and got: " + g1.equals( g3 ) );
+
+       System.out.println( "\n    Comparing equality of g3 and g4 [detect different length prior to detecting different digit]: " );
+       System.out.println( "      expecting: false\n" + "        and got: " + g3.equals( g4 ) );
+
+       System.out.println( "\n    Comparing equality of g1 and g5 [detect that negative is not equal]: " );
+       System.out.println( "      expecting: false\n" + "        and got: " + g1.equals( g5 ) );
+
+       System.out.println( "\n    =============================================\n" );
+
+       System.out.println( "\n    Testing addInt() method: ");
+
+       System.out.println( "\n    Adding g1 and g2: " );
+       System.out.println( "      expecting: 288255819439421328030184863004881699699012568297964152383652353106\n" +
+                           "        and got: " + g1.addInt( g2 ) );
+
+       System.out.println( "\n    Adding g1 and g4 [Test adding for different lengths]: " );
+       System.out.println( "      expecting: 144127909719710664015236559412160560513521376580484517041675682837\n" +
+                           "        and got: " + g1.addInt( g4 ) );
+
+
+
+       System.exit( 0 );
+
+    }
+ }
